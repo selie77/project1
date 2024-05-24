218d1f4d7a93915bbed680b045acbcfd3417a194
alazarsite <145592738+alazarsite@users.noreply.github.com>
2024-05-24 18:12:02 +0300

    this is the first  commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./header.jsx"
+import Note from "./note.jsx"
+import Footer from "./footer.jsx"
+import "./style.css"
+function App() {
+ 
+  return(
+ <div>
+  <Header/>
+  <Note/>
+  <Footer/>
+
+ </div>
+  )
+}
+
+export default App
---
+++ src/note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note ()
+{
+  return(
+    <div className="note">
+        <h1>
+            WELCOME 
+        </h1>
+        <p>
+            Hi this is EDA!
+        </p>
+    </div>
+  )
+}
+export default Note;
